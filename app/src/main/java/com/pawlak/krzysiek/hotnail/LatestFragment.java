7a68d98db287aff7e4af7687ebb8cce182d0b077
KrzysztofPawlak <krzysztof.pawlak.tech@gmail.com>
2019-07-28 07:25:53 +0200

    add 3 empty fragment to drawer menu

---
+++ app/src/main/java/com/pawlak/krzysiek/hotnail/LatestFragment.java
@@ -0,0 +1,19 @@
+package com.pawlak.krzysiek.hotnail;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class LatestFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_latest, container, false);
+    }
+}
--- app/src/main/java/com/pawlak/krzysiek/hotnail/MainActivity.java
+++ app/src/main/java/com/pawlak/krzysiek/hotnail/MainActivity.java
@@ -1,11 +1,20 @@
 package com.pawlak.krzysiek.hotnail;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +23,50 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HotFragment()).commit();
+            navigationView.setCheckedItem(R.id.hot);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.hot:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HotFragment()).commit();
+                break;
+            case R.id.latest:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new LatestFragment()).commit();
+                break;
+            case R.id.stats:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new StatsFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
